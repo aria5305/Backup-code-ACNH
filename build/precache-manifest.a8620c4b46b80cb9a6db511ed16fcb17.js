47bdd8c9ac63107717a444c8102422a91456db8b
aria5305 <tubbsv2@gmail.com>
2020-04-11 17:34:14 +1200

    Build folder - uploaded
    Now trying to deploy

---
+++ build/precache-manifest.a8620c4b46b80cb9a6db511ed16fcb17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b34c1a60e1094b861d9f9316a8c93d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c4720594583e838a5a9",
+    "url": "/static/css/main.b41ac1ab.chunk.css"
+  },
+  {
+    "revision": "52399b07597ed9fd8401",
+    "url": "/static/js/2.e6115955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6115955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4720594583e838a5a9",
+    "url": "/static/js/main.17723b75.chunk.js"
+  },
+  {
+    "revision": "68ea58658ca12cfc9443",
+    "url": "/static/js/runtime-main.4f2d62c3.js"
+  },
+  {
+    "revision": "4a2a519244aea75ded87ff7ca2e0b64b",
+    "url": "/static/media/Nook_Inc.4a2a5192.png"
+  },
+  {
+    "revision": "a14c5b98c62843b454954e61b0aed218",
+    "url": "/static/media/Tom_Nook.a14c5b98.png"
+  }
+]);
